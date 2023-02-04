20a4b26bede7c517f7054235d4c0faef5a8ad68c
Reece Roberts <Reece2.Roberts@live.uwe.ac.uk>
2023-02-04 20:31:14 +0000

    Health and damage functional

--- Roots/Assets/Scripts/PlayerManager.cs
+++ Roots/Assets/Scripts/PlayerManager.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    public GameObject player;
+
 
     #region [Varibles]
 
+    // GameObjects
+
+    public GameObject player;
+
+    public GameObject HP1;
+    public GameObject HP1_Empty;
+    public GameObject HP2;
+    public GameObject HP2_Empty;
+    public GameObject HP3;
+    public GameObject HP3_Empty;
+
     // int
     public int playerHealth;
+
+    public int countDownTick;
 
     // bool
     public bool isPlayerDamaged;
@@ -27,18 +40,53 @@ public class PlayerManager : MonoBehaviour
         playerHealth = 3;
 
         isPlayerDead = false;
+
+        countDownTick = 0;
     }
 
     // Update is called once per frame
     public void Update()
     {
-        if (isPlayerDamaged == true)
+
+        // Damage subset
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            playerHealth = -1;
+            playerHealth--;
+            Debug.Log("Check damage");
         }
+
+
+        //Health
+
+        if (playerHealth == 3)
+        {
+            HP1.SetActive(true);
+            HP1_Empty.SetActive(false);
+            HP2.SetActive(true);
+            HP2_Empty.SetActive(false);
+            HP3.SetActive(true);
+            HP3_Empty.SetActive(false);
+        }
+
+        if (playerHealth == 2)
+        {
+            HP3.SetActive(false);
+            HP3_Empty.SetActive(true);
+        }
+
+        if (playerHealth == 1)
+        {
+            HP2.SetActive(false);
+            HP2_Empty.SetActive(true);
+        }
+
+        // Dead player
 
         if (playerHealth == 0)
         {
+            HP1.SetActive(false);
+            HP1_Empty.SetActive(true);
+
             isPlayerDead = true;
         }
     }
