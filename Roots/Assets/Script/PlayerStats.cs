bcdf59b8eb3ae3e49a229d954213a4b043610a6d
Reece Roberts <Reece2.Roberts@live.uwe.ac.uk>
2023-02-04 17:30:55 +0000

    Added more UI and Scene management features
    I will next work on:
    
    Player health sprite visibility
    
    Buff tree current stats, for player to review
    
    Possibly other scene transitional functions & effects
    
    And maybe much more, but will need to check later.

---
+++ Roots/Assets/Script/PlayerStats.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+
+
+    #region [Varibles]
+
+    // int
+    public int playerHealth;
+
+    // bool
+    public bool isPlayerDamaged;
+
+    public bool isPlayerDead;
+
+    //Player buffs?
+
+
+    #endregion
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        playerHealth = 3;
+
+        isPlayerDead = false;
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (isPlayerDamaged == true)
+        {
+            playerHealth =- 1;
+        }
+
+        if (playerHealth == 0)
+        {
+            isPlayerDead = true;
+        }
+    }
+}
