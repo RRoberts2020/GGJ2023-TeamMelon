ead0dc063ed223ccbdea7d13b0001bbd0e899c5f
Jake Martin <Jake3.Martin@live.uwe.ac.uk>
2023-02-05 13:13:03 +0000

    Background added, infinite scrolling foreground

--- Roots/Assets/Scripts/ParalaxBehaviour.cs
+++ Roots/Assets/Scripts/ParalaxBehaviour.cs
@@ -4,19 +4,52 @@ using UnityEngine;
 
 public class ParalaxBehaviour : MonoBehaviour
 {
-    private MeshRenderer meshRenderer;
-    [SerializeField]
-    private float paralax_speed = 1f;
+
+    private Transform cameraTransform;
+    private Vector3 lastCameraPosition;
+    public Vector2 paralaxEffectMultiplier;
+    private float textureUnitSizeX;
+    private float textureUnitSizeY;
+    public bool infiniteHorizontal;
+    public bool infiniteVertical;
 
 
     void Start()
     {
-        meshRenderer = GetComponent<MeshRenderer>();
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition = cameraTransform.position;
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
+        textureUnitSizeY = texture.height / sprite.pixelsPerUnit;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        meshRenderer.material.mainTextureOffset += new Vector2(paralax_speed * Time.deltaTime, 0);
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position += new Vector3(deltaMovement.x * paralaxEffectMultiplier.x, deltaMovement.y * paralaxEffectMultiplier.y);
+        lastCameraPosition = cameraTransform.position;
+
+
+        if(infiniteHorizontal)
+        {
+            if (Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX)
+            {
+                float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+                transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y);
+            }
+        }
+
+       
+        if(infiniteVertical)
+        {
+            if (Mathf.Abs(cameraTransform.position.y - transform.position.y) >= textureUnitSizeY)
+            {
+                float offsetPositionY = (cameraTransform.position.y - transform.position.y) % textureUnitSizeY;
+                transform.position = new Vector3(transform.position.x, cameraTransform.position.y + offsetPositionY);
+            }
+        }
     }
 }
