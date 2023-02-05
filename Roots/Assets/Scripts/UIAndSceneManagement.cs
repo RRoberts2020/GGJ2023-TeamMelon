3533ba9be6ae923b84a1927c8f0d64447a2cc4e9
Reece Roberts <Reece2.Roberts@live.uwe.ac.uk>
2023-02-05 16:09:35 +0000

    Added buff tree functionality
    Still need to sync up with buttons

--- Roots/Assets/Scripts/UIAndSceneManagement.cs
+++ Roots/Assets/Scripts/UIAndSceneManagement.cs
@@ -9,9 +9,29 @@ public class UIAndSceneManagement : MonoBehaviour
 
     #region [Variables]
 
+    // int
+
+    public int buffAllocator;
+
     // Bool
 
     PlayerManager isPlayerDeadCheck;
+
+    public bool jumpBuff1Bool;
+
+    public bool speedBuff1Bool;
+
+    public bool jumpBuff2Bool;
+
+    public bool speedBuff2Bool;
+
+    public bool jumpBuff3Bool;
+
+    public bool speedBuff3Bool;
+
+    public bool jumpBuff4Bool;
+
+    public bool speedBuff4Bool;
 
     // States
 
@@ -25,6 +45,24 @@ public class UIAndSceneManagement : MonoBehaviour
 
     public GameObject GameOverScreen;
 
+    // Gameobject
+
+    public GameObject jumpBuff1;
+
+    public GameObject speedBuff1;
+
+    public GameObject jumpBuff2;
+
+    public GameObject speedBuff2;
+
+    public GameObject jumpBuff3;
+
+    public GameObject speedBuff3;
+
+    public GameObject jumpBuff4;
+
+    public GameObject speedBuff4;
+
     #endregion
 
     #region [Functions]
@@ -36,7 +74,25 @@ public class UIAndSceneManagement : MonoBehaviour
         BuffTreeScreen.SetActive(false);
         ChooseScreen.SetActive(false);
         GameOverScreen.SetActive(false);
-    }
+
+        buffAllocator = 1;
+
+        jumpBuff1Bool = false;
+
+        speedBuff1Bool = false;
+
+        jumpBuff2Bool = false;
+
+        speedBuff2Bool = false;
+
+        jumpBuff3Bool = false;
+
+        speedBuff3Bool = false;
+
+        jumpBuff4Bool = false;
+
+        speedBuff4Bool = false;
+}
 
     private void Update()
     {
@@ -67,6 +123,71 @@ public class UIAndSceneManagement : MonoBehaviour
     #endregion
 
     #region [BuffTree Screen]
+
+    public void BuffUI()
+    {
+
+        //Section 1
+        if (buffAllocator == 1 && jumpBuff1Bool == true)
+        {
+            jumpBuff1.SetActive(true);
+
+            speedBuff1.SetActive(false);
+        }
+
+        if (buffAllocator == 1 && speedBuff1Bool == true)
+        {
+            jumpBuff1.SetActive(false);
+
+            speedBuff1.SetActive(true);
+        }
+
+        //Section 2
+        if (buffAllocator == 2 && jumpBuff2Bool == true)
+        {
+            jumpBuff2.SetActive(true);
+
+            speedBuff2.SetActive(false);
+        }
+
+        if (buffAllocator == 2 && speedBuff2Bool == true)
+        {
+            jumpBuff2.SetActive(false);
+
+            speedBuff2.SetActive(true);
+        }
+
+        //Section 3
+        if (buffAllocator == 3 && jumpBuff3Bool == true)
+        {
+            jumpBuff3.SetActive(true);
+
+            speedBuff3.SetActive(false);
+        }
+
+        if (buffAllocator == 3 && speedBuff3Bool == true)
+        {
+            jumpBuff3.SetActive(false);
+
+            speedBuff3.SetActive(true);
+        }
+
+        //Section 4
+        if (buffAllocator == 4 && jumpBuff4Bool == true)
+        {
+            jumpBuff4.SetActive(true);
+
+            speedBuff4.SetActive(false);
+        }
+
+        if (buffAllocator == 4 && speedBuff4Bool == true)
+        {
+            jumpBuff4.SetActive(false);
+
+            speedBuff4.SetActive(true);
+        }
+
+    }
 
     #endregion
 
